b6083e69b8e6b4b67209a6ffca51cb3f8ae5e151
Diego Estrada Talamantes <a01634310@itesm.mx>
2019-12-01 22:16:34 -0600

    Comentary and cleanup

--- RoundRobin.py
+++ RoundRobin.py
@@ -1,9 +1,11 @@
 from tabulate import tabulate
 
+
 scheduling = ''
 quantum = 0
 terminacion = 0
 str_eventos = []
+
 
 def read_file():
     eventos = []
@@ -17,37 +19,36 @@ def read_file():
             prio = 0
             if scheduling == 'prioNonPreemptive': prio = int(next_line.split()[4])
             eventos.append({
-                'Tiempo':int(next_line.split()[0]),
-                'Accion':next_line.split()[1],
-                'PID':next_line.split()[2],
-                'Prio':int(prio)
+                'Tiempo': int(next_line.split()[0]),
+                'Accion': next_line.split()[1],
+                'PID': next_line.split()[2],
+                'Prio': int(prio)
             })
             next_line = f.readline().strip()
         str_eventos.append(next_line)
         terminacion = int(next_line.split()[0])
     return eventos
 
+
+# CPU Scheduling mediante RoundRobin
 def RoundRobin(quantum, eventos):
-    listos = []
-    bloqueados = []
-    terminados = []
-    cpu = { 'PID':'', 'entrada':0 }
-    output_table = [['Evento','Cola de listos','CPU','Bloqueados','Terminados']]
+    listos = []  # Procesos en la cola de listos
+    bloqueados = []  # Procesos bloqueados por algún I/O
+    terminados = []  # Procesos finalizados por el CPU
+    cpu = {'PID': '', 'entrada': 0}  # Proceso actual que corre el CPU y cuándo comenzó
+    output_table = [['Evento', 'Cola de listos', 'CPU', 'Bloqueados', 'Terminados']]
     specs_table = [['Proceso', 'Tiempo de llegada', 'Tiempo de terminación', 'Tiempo de CPU', 'Tiempo de espera', 'Turnaround', 'Tiempo de I/O']]
 
-
-    # Primer evento
-    e = eventos.pop(0)
-    benchmark = {}
-    str_evento = ''
-    for t in range(0,terminacion):
-        str_evento = ''
-        special = False
+    benchmark = {}  # Aquí colocaremos la info de nuestros procesos
+    e = eventos.pop(0)  # Primer evento
+    for t in range(0, terminacion):
+        str_evento = ''  # Será el mensaje a colocar en la tabla en "Evento"
+        special = False  # Determina si en esta unidad de tiempo ocurrió algo
         if cpu['PID']: benchmark[cpu['PID']][3] += 1
         for p in listos: benchmark[p][4] += 1
         for p in bloqueados: benchmark[p][6] += 1
 
-        # Si llega algún evento
+        # Si llega algún evento en esta unidad de tiempo
         if t == e['Tiempo']:
             special = True
             accion = e['Accion']
@@ -78,14 +79,14 @@ def RoundRobin(quantum, eventos):
 
             if len(eventos) > 0: e = eventos.pop(0)
 
-        # Se acaba el quantum del proceso y lo regresamos a la cola de listos
+        # Si se acaba el quantum del proceso lo regresamos a la cola de listos
         if t == cpu['entrada']+quantum:
             special = True
             listos.insert(0, cpu['PID'])
             cpu['PID'] = ''
             cpu['entrada'] = 0
 
-        # Si el CPU está libre metemos el proceso a la cola de listos
+        # Si el CPU está libre le metemos el último proceso de la cola de listos
         if cpu['PID'] == '' and len(listos) > 0:
             special = True
             if str_evento == '': str_evento = str(t) + ' quantum'
@@ -103,15 +104,18 @@ def RoundRobin(quantum, eventos):
 
     print(tabulate(output_table))
 
+    # Crear el specs_table con base en los diferentes benchmarks de cada proceso
     for p in sorted(benchmark):
         benchmark[p][5] = benchmark[p][2] - benchmark[p][1]
         specs_table.append(benchmark[p])
     print(tabulate(specs_table))
 
 
-def PriorityNotPreentive(eventos):
+# CPU Scheduling mediante PriorityNotPreemtive
+def PriorityNotPreemtive(eventos):
     print('Wenas')
+
 
 eventos = read_file()
 if scheduling == 'RR': RoundRobin(quantum, eventos)
-elif scheduling == 'prioNotPreentive': PriorityNotPreentive(eventos)
+elif scheduling == 'prioNotPreentive': PriorityNotPreemtive(eventos)
